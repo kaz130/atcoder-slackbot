33dbdab1b724c51733cb16b0113004c20fb657ba
Kazuki Aizawa <kaz130@users.noreply.github.com>
2019-08-20 17:06:53 +0900

    Add post_upcoming_contests method

--- main.py
+++ main.py
@@ -2,9 +2,27 @@
 
 import datetime
 from requests_html import HTMLSession
+from slack_webhooks import SlackWebhooks
 
 def main():
-    pass
+    post_upcoming_contests()
+
+def post_upcoming_contests():
+    slack = SlackWebhooks()
+    contests = get_upcoming_contests()
+    message = "予定されたコンテスト\n"
+    for i, contest in enumerate(contests):
+        days = ["(月)", "(火)", "(水)", "(木)", "(金)", "(土)", "(日)"]
+        message += contest['start_time'].strftime("%m/%d") \
+        + days[contest['start_time'].weekday()] \
+        + " " + contest['contest_name']
+        if (i != len(contests) - 1):
+            message += "\n"
+
+    print(message)
+    payload = {"text" : message}
+    slack.post(payload)
+
 
 def get_upcoming_contests():
     session = HTMLSession()
